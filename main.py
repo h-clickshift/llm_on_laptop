7f234aa7aca81cac5757b0c06f8a75246de5fb47
hugh <117718271+h-clickshift@users.noreply.github.com>
2024-06-20 17:04:47 +0800

    Update main.py
    format 🧑‍🏭

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import os,torch
+import os, torch
 import multiprocessing
 from res_profile import *
-def load_model(path, device):
+
+
+def load_model(path, device, dtype):
     cache_dir = os.path.join(os.path.dirname(__file__), path.split("/")[-1])
-    model = AutoModelForCausalLM.from_pretrained(path, cache_dir=cache_dir, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(path, cache_dir=cache_dir, trust_remote_code=True, torch_dtype=dtype)
     model.to(torch.device(device))
+    # for name, parm in model.named_parameters():
+    #     print(f"{name} - {parm.dtype} ")
     tokenizer = AutoTokenizer.from_pretrained(path, cache_dir=cache_dir)
     return model, tokenizer
+
 
 def generate(input_text, model, tokenizer, device):
     input_ids = tokenizer(input_text, return_tensors="pt").to(torch.device(device))
@@ -17,15 +22,19 @@ def generate(input_text, model, tokenizer, device):
     t1 = time.time()
     print(f"** time cost:{t1 - t0}s **")
     num_tokens = len(outputs[0])
-    print(f"** time per token: {num_tokens/(t1 - t0)} **")
+    print(f"** time per token: {num_tokens / (t1 - t0)} **")
     print(tokenizer.decode(outputs[0]))
 
 
-def run(pid, model, device, input_text):
-    plot_process = multiprocessing.Process(target=update_plot, args=(pid,model))
+def run(pid, model, device, input_text, dtype=torch.float32):
+    plot_process = multiprocessing.Process(target=update_plot, args=(pid, model))
     plot_process.start()
-    model, tok = load_model(model, device)
-    generate(input_text, model, tok, device)
+    try:
+        model, tok = load_model(model, device, dtype)
+        generate(input_text, model, tok, device)
+    except RuntimeError as e:
+        print(f"** Error:{e} **")
+        plot_process.kill()
     plot_process.kill()
 
 
@@ -34,6 +43,6 @@ if __name__ == "__main__":
     print(f"** Current Process ID: {pid} **")
     # run(pid, "google/gemma-7b", "mps", "HI,I am a language model.")
     # run(pid, "mistralai/Mistral-7B-v0.3", "mps", "HI,I am a language model.")
-    run(pid, "microsoft/Phi-3-mini-128k-instruct", "mps", "HI,I am a language model.")
+    run(pid, "microsoft/Phi-3-mini-128k-instruct", "cpu",
+        "HI,I am a language model.")  # torch.float16 get RuntimeError: "addmm_impl_cpu_" not implemented for 'Half'
     print("done")
-
